25d243a148fe7e3a7bbde74d3574bb215c9e7d3e
Ian Langworth ☠ <ian@langworth.com>
2014-04-08 21:19:02 -0700

    Look for cached CoffeeScript files like node-coffee-cache

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var fs = require('fs');
 var path = require('path');
 var coffee = require('coffee-script');
+var mkpath = require('mkpath');
 var EventEmitter = require('events').EventEmitter;
 
 var wrap = require('./lib/wrap');
 var watch = require('./lib/watch');
+
+var log = function(arg1, arg2, arg3, arg4, arg5, arg6) {
+    if (!process.env.BROWSERIFY_DEBUG) return;
+    console.log(new Date().toISOString() + " [browserify] " +
+                [arg1, arg2, arg3, arg4, arg5, arg6].join(' '));
+}
 
 function idFromPath (path) {
     return path.replace(/\\/g, '/');
@@ -51,10 +59,45 @@ var exports = module.exports = function (entryFile, opts) {
         debug : opts.debug,
         exports : opts.exports,
     };
+
+    log("Wrapping entry point: " + opts.entry);
     var w = wrap(opts_);
     w.register('.coffee', function (body, file) {
         try {
-            var res = coffee.compile(body, { filename : file });
+            // Check coffee-cache for the file first.
+            // See https://github.com/FogCreek/node-coffee-cache
+            var cacheDir = process.env['COFFEE_CACHE_DIR'] || '.coffee';
+            var cachePath = path.join(cacheDir, path.relative('.', file)).replace(/\.coffee$/, '.js');
+            var mapPath = path.resolve(cachePath.replace(/\.js$/, '.map'));
+            var res;
+
+            try {
+                var sourceTime = fs.statSync(file).mtime;
+                var cacheTime = fs.statSync(cachePath).mtime;
+                if (cacheTime > sourceTime) {
+                    log("Using cache for " + file);
+                    res = fs.readFileSync(cachePath, 'utf8');
+                }
+            } catch (e) {}
+
+            if (!res) {
+                // Added instrumentation for Artillery.
+                t = process.hrtime();
+                var compiled = coffee.compile(body, { filename : file, sourceMap: true });
+                t = process.hrtime(t);
+                delta = Math.round(t[0] * 1000 + t[1] / 1e6);
+                log("Compiled " + file + " in " + delta + " ms");
+
+                res = compiled.js;
+
+                // Handle older version of CoffeeScript.
+                if (res == null) res = compiled;
+
+                mkpath.sync(path.dirname(cachePath));
+                fs.writeFileSync(cachePath, res, 'utf8');
+                if (mapPath) fs.writeFileSync(mapPath, compiled.v3SourceMap, 'utf8');
+            }
+
         }
         catch (err) {
             w.emit('syntaxError', err);
@@ -180,7 +223,9 @@ var exports = module.exports = function (entryFile, opts) {
     self.modified = new Date;
 
     self.bundle = function () {
-        if (w._cache) return w._cache;
+        if (w._cache) {
+            return w._cache;
+        }
 
         var src = w.bundle.apply(w, arguments);
         self.ok = Object.keys(w.errors).length === 0;
